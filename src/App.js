92089c31daa3980ebb50d9b259aee4f4164e338f
Aditya <akaditya394@gmail.com>
2022-02-07 23:24:55 +0530

    meals sumarry added; available meals list added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
+import Meals from "../src/components/Meals/Meals.jsx";
 import Header from "../src/components/UI/Header/Header.jsx";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <Meals />
     </Fragment>
   );
 };
