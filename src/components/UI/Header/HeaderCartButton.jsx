f43c4b3b9e83d56ce61e07dc84653dcef2f314b6
Aditya <akaditya394@gmail.com>
2022-02-12 23:51:06 +0530

    cart context in use

--- src/components/UI/Header/HeaderCartButton.jsx
+++ src/components/UI/Header/HeaderCartButton.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../../Cart/CartIcon/CartIcon.jsx";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfItemsInCart = cartCtx.items.reduce((curNum, item) => {
+    return curNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItemsInCart}</span>
     </button>
   );
 };
